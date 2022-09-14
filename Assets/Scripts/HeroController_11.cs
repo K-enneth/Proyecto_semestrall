d32bc3dfff9efc332080f3bdceca150cba159ffd
Kenneth <alcanamay@gmail.com>
2022-09-14 17:39:42 -0500

    lloro
    el scrip funciona pero no es mio XDDD

---
+++ Assets/Scripts/HeroController_11.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class HeroController_11 : MonoBehaviour
+{
+    [Header("Animation Variable")]
+    [SerializeField] AnimatorController_1 animatorController;
+
+    [Header("Checker Variables")]                                //Cabecera del ComboBox "Variables"  //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
+    [SerializeField] LayerChecker_1 footA;                  //Instanciamento a la Clase "LayerChecker_1" = footA
+    [SerializeField] LayerChecker_1 footB;                  //Instanciamento a la Clase "LayerChecker_1" = footB
+
+    [Header("Boolean Variables")]                           //"Pestaña" con título en el Inspector 
+    public bool canDoubleJump;                              //variable boleana(verdadero/falso) para ejecutar el salto doble
+
+    [Header("Interruption Variables")]                      //"Pestaña" con título en el Inspector              
+    public bool canCheckGround;                             //Variable booleana, usada para detectar si tocas el piso
+
+
+
+
+    [Header("Rigid Variables")]
+    [SerializeField] private float doubleJumpForce;         //Agregamos una variable flotante para agrear furza al DobleSalto
+    [SerializeField] private float jumpForce;               //Agregamos una variable flotante para agrear furza al salto
+    [SerializeField] private float speed_;                  //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
+    [SerializeField] private Vector2 movementDirection;     //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
+
+    private Rigidbody2D rigidbody2D_;                       //Variable de instanciamiento
+    private bool jumpPressed = false;                       //variable usadas para saber si se apretó la barra espaciadora
+                                                            //y es personaje saltó.
+    private bool playerIsOnGround;                          //Variable privada tipo Bool, el Heroe esta tocando el piso?
+
+
+    void Start()
+    {
+        canCheckGround = true;                              //inicializamos la variable "canCheckGround" como verdadera
+        rigidbody2D_ = GetComponent<Rigidbody2D>();         //Instanciando la variable.
+        animatorController.Play(AnimationId.Idle);
+        //jumpPressed = Input.GetButtonDown("Jump");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleIsGrounding();                                 //Invoca al método "HandleIsGrounding" (El héroe está tocando el piso?). 
+        HandleControls();                                    //invocando el método "HandleControls" (abre el puerto de entrada del teclado)
+        HandleMovement();                                    //invocando el método "HandleMovement" (multiplica el valor de "x" por "speed".
+        HandleFlip();
+        HandleJump();                                         //invocando el método "HandleFlip" (rota el personaje a la izquierda o a la derecha)
+        
+    }
+
+    void HandleIsGrounding()
+    {
+        if (!canCheckGround) return;   //Si NO esta tocando el piso NO se ejecuta nada de este método
+                                       //(esta variable se apaga en la corrutina)
+        Debug.Log(!canCheckGround);
+
+
+        playerIsOnGround = footA.isTouching || footB.isTouching;  //Falta comentar..........
+        Debug.Log("Tocando el pioo");
+    }
+
+void HandleControls()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        jumpPressed = Input.GetButtonDown("Jump");
+
+    }
+
+    void HandleMovement()
+    {
+        rigidbody2D_.velocity = new Vector2(movementDirection.x * speed_, rigidbody2D_.velocity.y);
+
+        if (playerIsOnGround)
+        {
+            
+            if (Mathf.Abs(rigidbody2D_.velocity.x) > 0)                         //comprobamos si se esta moviendo en el eje "X"
+            {
+                animatorController.Play(AnimationId.Run);
+            }
+            else
+            {
+                animatorController.Play(AnimationId.Idle);
+            }
+        }
+
+    }
+    void HandleFlip()
+    {
+        if (rigidbody2D_.velocity.magnitude > 0)                //Sólo si el personaje se está moviendo ejecuta estas lineas...
+        {
+            if (rigidbody2D_.velocity.x >= 0)                           //si la velocidad en "x" es mayor que cero ejecuta la siguiente linea....
+            {
+                this.transform.rotation = Quaternion.Euler(0, 0, 0);            //No rotes
+            }
+            else                                                                            //de otro modo ejecuta las siguientes lineas.....
+            {
+                this.transform.rotation = Quaternion.Euler(0, 180, 0);              //rota en "y" 180º
+            }
+        }
+    }
+
+    void HandleJump()           //Método para agregarle fuerza la RigidBody del Hero
+     {
+        if (canDoubleJump && jumpPressed && !playerIsOnGround)  //"!playerIsOnGround" esta variable nos indica que NO esta tocando el piso
+        {                                                       
+
+            this.rigidbody2D_.velocity = Vector2.zero;
+            this.rigidbody2D_.AddForce(Vector2.up * doubleJumpForce, ForceMode2D.Impulse);//agrega impulso de fuerza instantánea hacia arriba al doble salto           
+            canDoubleJump = false;                                                        //apagamos la variable "canDoubleJump“ para que no brinque infinitamente
+        }
+
+        if (jumpPressed && playerIsOnGround)
+            
+        {
+             this.rigidbody2D_.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+             animatorController.Play(AnimationId.Idle);
+
+            StartCoroutine(HandleJumpAnimation());
+            canDoubleJump = true;                                                   //prendemos la variable "canDoubleJump" para que brinque
+                                                                                    //de nuevo si apretamos la barra espaciadora 
+
+        }
+    }
+
+    IEnumerator HandleJumpAnimation()                       //Corrutina que ejecuta dos "clips" desfasados en tiempo 0.4f unidades de tiempo
+    {
+        canCheckGround = false;
+        playerIsOnGround = false;
+        yield return new WaitForSeconds(0.1f);                  //Ejecutar el Clip "Brinco" durante 0.1f uniades de tiempo
+        animatorController.Play(AnimationId.PrepararBrinco);   //Ejecuta el clip "PrepararBrinco"
+        yield return new WaitForSeconds(0.2f);                  //Ejecutar el Clip "PrepararBrinco" durante 0.2f uniades de tiempo
+        animatorController.Play(AnimationId.Brincar);           //Ejecuta el clip "Brinco"
+        //yield return new WaitForSeconds(1);                     //Ejecutar el Clip "Brinco" durante 1f uniades de tiempo
+        canCheckGround = true;
+    }
+
+}
